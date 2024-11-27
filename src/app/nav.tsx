44497a5e366774111fde4d2129f2c14bd16ca8af
Tyler Weeres <tweeres04@gmail.com>
2024-11-27 10:11:12 -0800

    Added a homepage so we can nav still when errors

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,7 +1,8 @@
 export default function Nav() {
   return (
     <div className="flex gap-3 nav mb-10 text-blue-950">
-      <a href="/">Updates query</a>
+      <a href="/">Home</a>
+      <a href="/updates">Updates query</a>
       <a href="/no-query">No query</a>
       <a href="/no-next-options">No next options</a>
       <a href="/no-query-params">No query params</a>
