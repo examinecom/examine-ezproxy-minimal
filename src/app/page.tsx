5da4717de89b3b7561ca5952ca66583dc0e32ae8
Tyler Weeres <tweeres04@gmail.com>
2024-11-27 06:31:24 -0800

    Show some examine updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+function appendSearchParams(
+  url: URL,
+  paramKey: string,
+  values: string[]
+): void {
+  values.forEach((value) => {
+    url.searchParams.append(paramKey, value);
+  });
+}
 
-export default function Home() {
+async function getUpdates({
+  limit = 10,
+  types,
+  category,
+}: {
+  limit: number;
+  types: string;
+  category: string;
+}) {
+  const url = new URL(
+    `/v1/updates?per_page=${limit}`,
+    "https://api.examine.com"
+  );
+
+  if (types) {
+    const typesArray = types.split(",");
+    appendSearchParams(url, "types[]", typesArray);
+  }
+
+  if (category) {
+    const categoriesArray = category.split(",");
+    appendSearchParams(url, "category[]", categoriesArray);
+  }
+
+  const endpoint = decodeURIComponent(url.pathname + url.search);
+  const response = await fetch(`https://api.examine.com${endpoint}`, {
+    next: { tags: ["updates"], revalidate: 0 },
+  });
+
+  return response || null;
+}
+
+export default async function Home() {
+  const pageUpdatesPromise = getUpdates({
+    limit: 5,
+    types: "",
+    category: "tech,site,supplement_guide,content",
+  }).then((r) => r.json());
+  const studyUpdatesPromise = getUpdates({
+    limit: 5,
+    types: "",
+    category: "study_summary",
+  }).then((r) => r.json());
+
+  const [pageUpdates, studyUpdates] = await Promise.all([
+    pageUpdatesPromise,
+    studyUpdatesPromise,
+  ]);
+
+  console.log({ pageUpdates });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+    <div className="max-w-[900px] mx-auto">
+      <h1>Some Examine updates:</h1>
+      <h2>Page updates:</h2>
+      <ul className="mb-20">
+        {pageUpdates.data.map((update: any) => (
+          <li key={update.id}>
+            <h3
+              dangerouslySetInnerHTML={{
+                __html: update.content?.name ?? update.headline,
+              }}
+            ></h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: update.detailed_note ?? update.note,
+              }}
+            ></div>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))}
+      </ul>
+      <h2>Study updates:</h2>
+      <ul>
+        {studyUpdates.data.map((update: any) => (
+          <li key={update.date}>
+            <h3>{update.headline}</h3>
+            <ul>
+              {update.study_summaries.map((study: any) => (
+                <li
+                  key={study.id}
+                  dangerouslySetInnerHTML={{ __html: study.headline }}
+                ></li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
